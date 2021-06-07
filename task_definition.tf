ca861b08bfdd6c1ce59d37f51b4df2d8cb801f82
root <root@ip-172-31-49-203.ec2.internal>
2021-06-07 08:04:33 +0000

    initial commit

---
+++ task_definition.tf
@@ -0,0 +1,18 @@
+resource "aws_ecs_task_definition" "task_definition" {
+  family = "worker"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "service-first"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
